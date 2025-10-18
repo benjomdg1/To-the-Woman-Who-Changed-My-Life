776f322da66a2af7213b872efe76461754b6a6c2
benjomdg1 <benjomdg1@gmail.com>
2025-10-18 12:12:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const scroll = document.getElementById("scrollPaper");
+
+  setTimeout(() => {
+    scroll.classList.add("revealed");
+  }, 1000);
+});
